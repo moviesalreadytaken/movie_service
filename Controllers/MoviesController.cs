ae33467625e1710f6088f6fdcb0faf5319cdcc7f
alexalreadytaken <alex.shemelev22@gmail.com>
2022-06-14 21:17:42 +0500

    improvements

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -18,15 +18,6 @@ namespace movie.Controllers
         public MoviesController(MovieContext context)
         {
             _context = context;
-            _context.Add(new MovieModel(Guid.Empty, "Some film1", new DateOnly(2000, 10, 10), 13, "some film description", 4.6f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film2", new DateOnly(2001, 10, 10), 0, "some film description", 4.9f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film3", new DateOnly(2002, 10, 10), 16, "some film description", 3f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film4", new DateOnly(2003, 10, 10), 18, "some film description", 4f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film5", new DateOnly(2004, 10, 10), 21, "some film description", 4.1f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film6", new DateOnly(2005, 10, 10), 13, "some film description", 2.5f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film7", new DateOnly(2006, 10, 10), 16, "some film description", 4.1f, null));
-            _context.Add(new MovieModel(Guid.Empty, "Some film8", new DateOnly(2007, 10, 10), 0, "some film description", 1f, null));
-            _context.SaveChanges();
         }
 
         // GET: api/Movies
--- Models/MovieContext.cs
+++ Models/MovieContext.cs
@@ -6,6 +6,18 @@ public class MovieContext : DbContext
 {
     public MovieContext(DbContextOptions<MovieContext> options) : base(options)
     {
+        if (!MovieModels.Any())
+        {
+            Add(new MovieModel(Guid.Empty, "Some film1", new DateOnly(2000, 10, 10), 13, "some film description", 4.6f, null));
+            Add(new MovieModel(Guid.Empty, "Some film2", new DateOnly(2001, 10, 10), 0, "some film description", 4.9f, null));
+            Add(new MovieModel(Guid.Empty, "Some film3", new DateOnly(2002, 10, 10), 16, "some film description", 3f, null));
+            Add(new MovieModel(Guid.Empty, "Some film4", new DateOnly(2003, 10, 10), 18, "some film description", 4f, null));
+            Add(new MovieModel(Guid.Empty, "Some film5", new DateOnly(2004, 10, 10), 21, "some film description", 4.1f, null));
+            Add(new MovieModel(Guid.Empty, "Some film6", new DateOnly(2005, 10, 10), 13, "some film description", 2.5f, null));
+            Add(new MovieModel(Guid.Empty, "Some film7", new DateOnly(2006, 10, 10), 16, "some film description", 4.1f, null));
+            Add(new MovieModel(Guid.Empty, "Some film8", new DateOnly(2007, 10, 10), 0, "some film description", 1f, null));
+            SaveChanges();
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
